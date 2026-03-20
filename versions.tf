97cb101abd1ddc9f2679c548d2f9b3ee79c84232
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 15:51:37 -0400

    chore: polish module with basic example, changelog, and version fixes

--- versions.tf
+++ versions.tf
@@ -1,23 +1,12 @@
-##-----------------------------------------------------------------------------
-## Versions
-##-----------------------------------------------------------------------------
-# Terraform version
 terraform {
   required_version = ">= 1.10.0"
-}
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=4.11.0"
-    }
-    databricks = {
-      source  = "databricks/databricks"
-      version = ">=1.9.2"
+      version = "# Terraform version"
     }
   }
-
   provider_meta "azurerm" {
     module_name = "terraform-az-modules/terraform-azurerm-databricks"
   }
